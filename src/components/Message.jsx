9b38a9c0a56362bd9052ca4e068982e3d6ddf6af
Dvdrepetto <davidmrepetto@gmail.com>
2025-02-05 09:48:10 +0200

    done

---
+++ src/components/Message.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Message = ({ message }) => {
+    return (
+        <div>
+            <p className="message">{message}
+            </p>
+        </div>
+    )
+}
+export default Message;
+
